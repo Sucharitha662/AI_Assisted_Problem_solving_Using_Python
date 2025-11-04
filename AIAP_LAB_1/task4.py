216bde114ac5bd53b3317516289eaa89f143b06d
Sucharitha kondaparthi <sucharithakondaparthi@gmail.com>
2025-11-05 00:07:39 +0530

    Add AIAP_LAB_1 folder

---
+++ AIAP_LAB_1/task4.py
@@ -0,0 +1,17 @@
+#Recursive function to find factorial of a number
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+#Iterative function to find factorial of a number
+def factorial_iterative(n):
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
+
+#Example Usage
+print(factorial(5))
+print(factorial_iterative(5))
