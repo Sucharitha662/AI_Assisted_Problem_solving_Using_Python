93d768735d569ba860d6f5963fb0383c04214f30
Sucharitha kondaparthi <sucharithakondaparthi@gmail.com>
2025-11-08 00:23:36 +0530

    Remove old LAB_2 and assignment files

--- LAB_2/sum_of_squares.py
+++
@@ -1,8 +0,0 @@
-# Write a Python function to calculate the sum of squares of numbers in a list
-def sum_of_squares(numbers):
-
-    return sum(x**2 for x in numbers)
-
-# Test the function
-print(sum_of_squares([1, 2, 3, 4, 5]))
-
